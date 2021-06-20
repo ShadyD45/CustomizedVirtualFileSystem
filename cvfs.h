54a7037f06cad4688cf4ba05bc0d451fb756d58e
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2021-06-20 22:46:16 +0530

    rewrote code for object orientation

--- cvfs.h
+++ cvfs.h
@@ -1,33 +1,43 @@
-//#pragma once
-
 #ifndef CVFS_H
 #define CVFS_H
 
 #include "structs.h"
 
-// --------------- Utility Functions ------------------ //
-PINODE GetFreeInode();
-bool FileExists(char *, PPINODE node = NULL);
-int GetFDByName(char *);
-int GetFreeFD();
-bool HasReadPermission(int);
-bool HasWritePermission(int);
-void DisplayHelp();
-// --------------- Utility Functions ------------------ //
+class FileSystem
+{
+	private:
+		UFDT UFDTArr[UFDT_ARR_SIZE];
+		SuperBlock SuperBlockObj;
+		PINODE Head;
+		
+		// --------------- Utility Functions ------------------ //
+		PINODE GetFreeInode();
+		bool FileExists(char *, PPINODE node = NULL);
+		int GetFreeFD();
+		bool HasReadPermission(int);
+		bool HasWritePermission(int);
+		
+	
+	public:
+		FileSystem();			// Constructor
+		void DisplayHelp();
+		int GetFDByName(char *);
+		// --------------- Utility Functions ------------------ //
 
-// --------------- File System Functions --------------- //
-void CloseAllFiles();
-int CloseFile(char *);
-int stat_file(char *);
-void man(char *);
-int rm_file(char *);
-void ls_files();
-int OpenFile(char *, int);
-int ReadFile(int, char *, int);
-int WriteFile(int, char *, int);
-int CreateFile(char *, int);
-void CreateDILB();
-void InitialiseSuperBlock();
-// --------------- File System Functions --------------- //
 
+		// --------------- File System Functions --------------- //
+		void CloseAllFiles();
+		int CloseFile(char *);
+		int stat_file(char *);
+		void man(char *);
+		int rm_file(char *);
+		void ls_files();
+		int OpenFile(char *, int);
+		int ReadFile(int, char *, int);
+		int WriteFile(int, char *, int);
+		int CreateFile(char *, int);
+		void CreateDILB();
+		void InitialiseSuperBlock();
+		// --------------- File System Functions --------------- //
+};
 #endif
